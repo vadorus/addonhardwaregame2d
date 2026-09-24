c4c6bd192ab8a43aeba98c465808dcc5204f90e7
Vadorus <51049891+vadorus@users.noreply.github.com>
2026-09-24 19:09:17 +0200

    test: run first CPU runway scenario

--- tests/smoke_test.gd
+++ tests/smoke_test.gd
@@ -3,6 +3,7 @@ extends Node
 const CPU_DESIGN := preload("res://scripts/CpuDesign.gd")
 const GARAGE_SCENARIO := preload("res://tests/scenarios/GarageScenario.gd")
 const FIRST_CPU_JOURNEY_SCENARIO := preload("res://tests/scenarios/FirstCpuJourneyScenario.gd")
+const FIRST_CPU_RUNWAY_SCENARIO := preload("res://tests/scenarios/FirstCpuRunwayScenario.gd")
 const INDUSTRIALIZATION_GATE_SCENARIO := preload("res://tests/scenarios/IndustrializationGateScenario.gd")
 const PROTOTYPE_DECISION_SCENARIO := preload("res://tests/scenarios/PrototypeDecisionScenario.gd")
 const VALIDATION_DECISION_SCENARIO := preload("res://tests/scenarios/ValidationDecisionScenario.gd")
@@ -134,6 +135,10 @@ func _ready() -> void:
 	var first_cpu_journey_error := FIRST_CPU_JOURNEY_SCENARIO.run(self)
 	if first_cpu_journey_error != "":
 		_fail(first_cpu_journey_error)
+		return
+	var first_cpu_runway_error := FIRST_CPU_RUNWAY_SCENARIO.run()
+	if first_cpu_runway_error != "":
+		_fail(first_cpu_runway_error)
 		return
 	var prototype_decision_error := PROTOTYPE_DECISION_SCENARIO.run(self)
 	if prototype_decision_error != "":
